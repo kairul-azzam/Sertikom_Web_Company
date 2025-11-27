9db533fe1463c1df8856ae9aab106d16abc655dc
kairul-azzam <kaiixxyy.xpro@gmail.com>
2025-11-27 08:35:47 +0700

    serti

--- dist/script.js
+++ dist/script.js
@@ -5,14 +5,4 @@ window.addEventListener("scroll", () =>{
     navbar.classList.toggle("scrolling-active", posisi)
 })
 
-const container4 = document.querySelector('.container4');
-const registerBtn = document.querySelector('.register-btn');
-const loginBtn = document.querySelector('.login-btn');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add('active');
-})
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove('active');
-})
